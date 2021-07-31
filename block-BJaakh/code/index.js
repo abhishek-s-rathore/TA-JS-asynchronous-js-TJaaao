b1076e9c26e864af961f67593b5f5276033f1e4e
Abhishek <abhishek.990914@gmail.com>
2021-07-31 21:55:10 +0530

    Assignment Completed

---
+++ block-BJaakh/code/index.js
@@ -0,0 +1,71 @@
+let input = document.querySelector('input');
+let root = document.querySelector('ul');
+let myUrl = `https://sleepy-falls-37563.herokuapp.com/api/todo`;
+
+function handleDelete(event, todo, arr) {
+  console.log(event.target);
+  arr.forEach((elem) => {
+    if (todo._id === elem._id) {
+      console.log(todo._id, elem._id);
+
+      fetch(myUrl + `/${todo._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      fetchData(myUrl);
+    }
+  });
+}
+
+function displayUI(array) {
+  root.innerHTML = '';
+  array.forEach((elem, ind, arr) => {
+    let li = document.createElement('li');
+    let input = document.createElement('input');
+    input.type = `checkbox`;
+    input.checked = false;
+    let label = document.createElement('label');
+    label.innerText = elem.title;
+    let span = document.createElement('span');
+    span.innerText = `❌`;
+    span.addEventListener('click', (event) => handleDelete(event, elem, arr));
+    li.append(input, label, span);
+    root.append(li);
+  });
+}
+
+function fetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.todos);
+      displayUI(data.todos);
+    });
+}
+
+function handleInput(event, url) {
+  let value = event.target.value;
+  if (event.keyCode === 13 && value.trim('') !== '') {
+    let sentData = {
+      todo: {
+        title: value,
+        isCompleted: false,
+      },
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(sentData),
+    });
+
+    fetchData(url);
+  }
+}
+
+fetchData(myUrl);
+input.addEventListener('keyup', (event) => handleInput(event, myUrl));
